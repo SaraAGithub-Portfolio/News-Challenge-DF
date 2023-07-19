7468124f43cec98c3d78f1b538fcde60d7a315fb
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-07-19 13:52:30 +0100

    Made a components folder added components: Header & Footer.

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+const Header = () => {
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col-md-10 text-center">
+                    <h3>DF News</h3>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
