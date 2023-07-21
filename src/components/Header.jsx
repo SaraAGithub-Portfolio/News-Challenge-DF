94884acf3797d4aef404cc57c61877c6813e948e
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-07-21 13:00:11 +0100

    added some outlining for the header component.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
-const Header = () => {
+import PropTypes from 'prop-types';
+
+const Header = ({ title }) => {
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-10 text-center">
-                    <h3>DF News</h3>
+                <div className="col-md-12 text-center">
+                    <h3 className="animate-character" data-testid="header">{title}</h3>
                 </div>
             </div>
         </div>
     );
+};
+Header.propTypes = {
+    title: PropTypes.string.isRequired,
 };
 
 export default Header;
