7f83e17a71a47573d06a504f96ca39e8d851ef2a
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-07-21 09:43:10 +0100

    testing to see if I can make a GET request. Terminal says: GET / mockAPIResponse 200.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,40 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import axios from "axios";
+import { useState, useEffect } from 'react';
+import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Headlines from "./Components/Headlines";
+import mockNewsData from "../tests/mock data/mockNewsData.json";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [headlines, setHeadlines] = useState([]);
+
+  useEffect(() => {
+    getHeadlines();
+  }, []);
+
+  const getHeadlines = async () => {
+    try {
+      const response = await axios.get("http://localhost:5173/mockApiResponse/");
+      console.log(response);
+      setHeadlines(mockNewsData.data.results);
+    } catch (error) {
+      console.log('Error fetching headlines:', error);
+    }
+  };
+
+  if (headlines.length === 0) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Router>
+
+      <Routes>
+        <Route path="/" element={<Headlines headlines={headlines} />} />
+      </Routes>
+    </Router>
+
+  );
 }
 
-export default App
+export default App;
