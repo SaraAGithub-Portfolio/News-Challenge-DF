3eecf25be677baa176bd6ade4c74a437a98dbeb0
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-07-23 13:08:36 +0100

    had to make small change to dataservice.js as there was a typo with the get request.

--- src/Utils/DataService.js
+++ src/Utils/DataService.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const DataService = async () => {
     try {
-        const responseData = await axios.get('http://localhost3000/webTitle');
+        const responseData = await axios.get('http://localhost:3000/webTitle');
         return responseData.data;
     } catch (error) {
         console.error('Error fetching data:', error);
