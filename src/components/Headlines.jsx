a678d9e803374dd6c73b7ae9cfe6680538237c05
SaraAGithub-Portfolio <skausa29@gmail.com>
2023-07-26 17:47:11 +0100

    made styling adjustments to Headlines.jsx using Bootstrap.

--- src/components/Headlines.jsx
+++ src/components/Headlines.jsx
@@ -6,14 +6,16 @@ const Headlines = ({ headlines }) => {
         return <div>Please wait a moment...</div>
     }
     const newsHeadlines = headlines.map((newsData) => (
-        <div key={newsData.id}>
-            <h2>{newsData.fields.headline}</h2>
-            <img src={newsData.fields.thumbnail} alt={`Thumbnail ${newsData.id}`} />
+        <div key={newsData.id} className="card mb-4">
+            <img src={newsData.fields.thumbnail} alt={`Thumbnail ${newsData.id}`} className="card-img-top" />
+            <div className="card-body" >
+                <h2 className="card-title">{newsData.fields.headline}</h2>
+            </div>
         </div>
     ));
 
     return (
-        <div>
+        <div className="container">
             {newsHeadlines.length > 0 ? newsHeadlines : <div>Please wait a moment...</div>}
         </div>
     );
